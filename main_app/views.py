4d953abf3250b2629080b36e0971b73f0101acce
GreyA <grey.altaer@gmail.com>
2025-04-03 09:11:46 -0700

    second model added and rendered

--- main_app/views.py
+++ main_app/views.py
@@ -1,11 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Cat
+from .forms import FeedingForm
 
 # Create your views here.
 def cat_index(request):
     cats = Cat.objects.all()  # look familiar?
-    return render(request, 'cats/index.html', {'cats': cats})
+    return render(request, 'cats/index.html', {'cat': cats})
 
 def home(request):
     return render(request, 'home.html')
@@ -15,12 +16,15 @@ def about(request):
 
 def cat_index(request):
     # Render the cats/index.html template with the cats data
-    cat = Cat.objects.all()
-    return render(request, 'cats/index.html', {'cats': cat})
+    cats = Cat.objects.all()
+    return render(request, 'cats/index.html', {'cats': cats})
 
 def cat_detail(request, cat_id):
     cat = Cat.objects.get(id=cat_id)
-    return render(request, 'cat/detail.html', {'cat': cat})
+    feeding_form = FeedingForm()
+    return render(request, 'cats/detail.html', {
+        'cat': cat, 'feeding_form': feeding_form
+    })
 
 class CatCreate(CreateView):
     model = Cat
@@ -33,3 +37,13 @@ class CatUpdate(UpdateView):
 class CatDelete(DeleteView):
     model = Cat
     success_url = '/cats/'
+    
+def add_feeding(request, cat_id):
+    form = FeedingForm(request.POST)
+    
+    if form.is_valid():
+        new_feeding = form.save(commit=False)
+        new_feeding.cat_id = cat_id
+        new_feeding.save()
+        
+        return redirect('cat-detail', cat_id=cat_id)
